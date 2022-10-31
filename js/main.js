78b395ab3616309ec06cd27d381f85112586a6b6
MoNeN-92 <ika.tatishvili@gmail.com>
2022-10-31 23:07:27 +0400

    sasa

--- js/main.js
+++ js/main.js
@@ -7,14 +7,59 @@ navbar.forEach(Element => {
     })
 });
 
-// $(':radio').change(function() {
-//     console.log('New star rating: ' + this.value);
-//   });
 
-let myCardBtn =document.querySelector('.moviesky')
-let content = document.querySelector('.skycraper')
- 
-myCardBtn.addEventListener('click', function(e){
+
+let skyBtn =document.querySelector('.moviesky')
+let hangBtn =document.querySelector('.moviehang')
+let girlBtn =document.querySelector('.moviegirl')
+let deepbtn =document.querySelector('.moviedeep')
+let transBtn =document.querySelector('.movietrans')
+
+let skycrap = document.querySelector('.skycraper')
+let transformer = document.querySelector('.transformers')
+let hangover = document.querySelector('.hangover')
+let catGirl = document.querySelector('.girls')
+let deepwater = document.querySelector('.deepwater')
+
+
+
+skyBtn.addEventListener('click', function(e){
     e.preventDefault();
-    content.classList.toggle('d-none')
+    transformer.classList.add('d-none')
+    hangover.classList.add('d-none')
+    catGirl.classList.add('d-none')
+    deepwater.classList.add('d-none')
+    skycrap.classList.remove('d-none')
+})
+hangBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    transformer.classList.add('d-none')
+    hangover.classList.remove('d-none')
+    catGirl.classList.add('d-none')
+    deepwater.classList.add('d-none')
+    skycrap.classList.add('d-none')
+})
+girlBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    transformer.classList.add('d-none')
+    hangover.classList.add('d-none')
+    catGirl.classList.remove('d-none')
+    deepwater.classList.add('d-none')
+    skycrap.classList.add('d-none')
+})
+deepbtn.addEventListener('click', function(e){
+    e.preventDefault();
+    transformer.classList.add('d-none')
+    hangover.classList.add('d-none')
+    catGirl.classList.add('d-none')
+    deepwater.classList.remove('d-none')
+    skycrap.classList.add('d-none')
+})
+transBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    transformer.classList.remove('d-none')
+    hangover.classList.add('d-none')
+    catGirl.classList.add('d-none')
+    deepwater.classList.add('d-none')
+    skycrap.classList.add('d-none')
 })
